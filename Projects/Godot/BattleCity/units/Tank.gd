a5166b963aa3bf1aae929a3972de941ad0083b97
hilfazer <az13337@gmail.com>
2017-02-24 23:27:13 +0100

    animations almost work

--- Projects/Godot/BattleCity/units/Tank.gd
+++ Projects/Godot/BattleCity/units/Tank.gd
@@ -4,6 +4,7 @@ extends Node2D
 const COLOR_OFFSET = { GOLD = 0, SILVER = 8, GREEN = 200, PURPLE = 208 }
 const TYPE_OFFSET = { MK1 = 0, MK2 = 25, MK3 = 50, MK4 = 75, MK5 = 100, MK6 = 125, MK7 = 150, MK8 = 175 }
 const DIRECTION_OFFSET = { UP = 0, LEFT = 2, DOWN = 4, RIGHT = 6 }
+
 const BULLET_PATH = "res://units/Bullet.tscn"
 const SPEED = 40
 const FIRING_DELAY = .3
@@ -18,14 +19,13 @@ var stage
 func _ready():
 	set_process( true )
 	set_fixed_process( true )
-	cannonEndDistance = abs( self.get_node("CannonEnd").get_pos().y )
+	m_cannonEndDistance = abs( self.get_node("CannonEnd").get_pos().y )
 	
 	var spriteFrame = get_node("Sprite").get_frame()
 	if ( spriteFrame >= COLOR_OFFSET.PURPLE ): setColor( COLOR_OFFSET.PURPLE )
 	elif ( spriteFrame >= COLOR_OFFSET.GREEN ): setColor( COLOR_OFFSET.GREEN )
 	elif ( spriteFrame >= COLOR_OFFSET.SILVER ): setColor( COLOR_OFFSET.SILVER )
 	else: setColor( COLOR_OFFSET.GOLD )
-	setTankType( TYPE_OFFSET.MK1 )
 	
 	self.rotateToDirection( 8 )
 	stage = get_parent()
@@ -46,10 +46,8 @@ var m_typeFrame = TYPE_OFFSET.MK1
 
 func setTankType( type ):
 	m_typeFrame = type
-	#todo set animations
+	addAnimations( m_colorFrame, m_typeFrame )
 
-
-var m_motion = MOTION.NONE
 
 func processMovement( delta ):
 	var body = get_node("Body2D")
@@ -58,6 +56,8 @@ func processMovement( delta ):
 	self.set_pos( get_pos() + body.get_pos() )
 	body.set_pos( Vector2(0,0) )
 
+
+var m_motion = MOTION.NONE
 
 func setMotion( motionVector2d ):
 	m_motion = motionVector2d
@@ -72,14 +72,22 @@ var m_colorFrame
 func setColor( color ):
 	assert ( color in [COLOR_OFFSET.GOLD,COLOR_OFFSET.SILVER,COLOR_OFFSET.GREEN,COLOR_OFFSET.PURPLE] )
 	m_colorFrame = color
-	
-	var animation = get_node("Sprite/AnimationPlayer").get_animation("DriveUp")
-	var trackIdx = animation.find_track(".:frame")
-	#var 
-	
-	for keyIdx in range(0, animation.track_get_key_count( trackIdx ) ):
-		animation.track_set_key_value(trackIdx, keyIdx, 9)
-	
+	addAnimations( m_colorFrame, m_typeFrame )
+
+
+func addAnimations(colorFrame, tankTypeFrame):
+	for directionFrame in DIRECTION_OFFSET:
+		var firstFrame = m_colorFrame + m_typeFrame + DIRECTION_OFFSET[directionFrame]
+		if ( firstFrame in m_frameToAnimationName ):
+			continue
+
+		var animationToAdd = get_node("Sprite/AnimationPlayer").get_animation("Drive").duplicate()
+		var trackIdx = animationToAdd.find_track(".:frame")
+		for keyIdx in range(0, animationToAdd.track_get_key_count( trackIdx ) ):
+			animationToAdd.track_set_key_value( \
+				trackIdx, keyIdx, firstFrame + keyIdx)
+		get_node("Sprite/AnimationPlayer").add_animation("Drive"+str(firstFrame), animationToAdd)
+		m_frameToAnimationName[firstFrame] = "Drive"+str(firstFrame)
 
 
 func processRotation():
@@ -102,26 +110,30 @@ func rotateToDirection( direction ):
 	m_rotation = direction
 	var direction2frame = { 2 : 4, 4 : 2, 6 : 6, 8 : 0 }
 	var sprite = get_node("Sprite").set_frame( m_colorFrame + direction2frame[m_rotation] )
-	
-	if ( m_rotation == 2 ):
-		self.get_node("CannonEnd").set_pos( Vector2( 0, cannonEndDistance ) )
-	elif ( m_rotation == 4 ):
-		self.get_node("CannonEnd").set_pos( Vector2( -cannonEndDistance, 0 ) )
-	elif ( m_rotation == 6 ):
-		self.get_node("CannonEnd").set_pos( Vector2( cannonEndDistance, 0 ) )
-	elif ( m_rotation == 8 ):
-		self.get_node("CannonEnd").set_pos( Vector2( 0, -cannonEndDistance ) )
+	m_currrentAnimationName = m_frameToAnimationName[m_colorFrame + m_typeFrame + direction2frame[m_rotation]]
 
+	if ( m_rotation == 2 ):
+		self.get_node("CannonEnd").set_pos( Vector2( 0, m_cannonEndDistance ) )
+	elif ( m_rotation == 4 ):
+		self.get_node("CannonEnd").set_pos( Vector2( -m_cannonEndDistance, 0 ) )
+	elif ( m_rotation == 6 ):
+		self.get_node("CannonEnd").set_pos( Vector2( m_cannonEndDistance, 0 ) )
+	elif ( m_rotation == 8 ):
+		self.get_node("CannonEnd").set_pos( Vector2( 0, -m_cannonEndDistance ) )
+
+
+var m_frameToAnimationName = {}
+var m_currrentAnimationName = ""
 
 func processAnimation():
 	if ( m_motion == MOTION.NONE):
 		get_node("Sprite/AnimationPlayer").stop()
 	elif ( get_node("Sprite/AnimationPlayer").is_playing() == false ):
-		get_node("Sprite/AnimationPlayer").play("DriveUp")
-		
+		get_node("Sprite/AnimationPlayer").play( m_currrentAnimationName )
+
 
 var m_firingCooldown = 0.0
-var cannonEndDistance = 0
+var m_cannonEndDistance = 0
 
 func fireCannon():
 	if m_firingCooldown > 0.0:
