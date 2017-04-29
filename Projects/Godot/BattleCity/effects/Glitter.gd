dc6bcebdf0fba252b9e167468dba93799f05b4df
hilfazer <az13337@gmail.com>
2017-04-29 20:08:15 +0200

    resetting glitter time

--- Projects/Godot/BattleCity/effects/Glitter.gd
+++ Projects/Godot/BattleCity/effects/Glitter.gd
@@ -1,20 +1,32 @@
 extends Node2D
 
-var m_glitterTimer
+var m_glitterTimer = Timer.new() setget deleted, deleted
 
 signal finished
 
+
+func deleted():
+	assert(false)
+
+
+func _ready():
+	add_child(m_glitterTimer)
+
+
 func glitterForSeconds(seconds):
 	var animationPlayer = get_node("Sprite/AnimationPlayer")
-	m_glitterTimer = Timer.new()
 	m_glitterTimer.set_wait_time( seconds )
+	if animationPlayer.is_playing():
+		m_glitterTimer.start()
+		return
+
 	m_glitterTimer.set_one_shot(true)
 	m_glitterTimer.connect( "timeout", animationPlayer, "stop" )
 	m_glitterTimer.connect( "timeout", self, "stopGlittering" )
 	animationPlayer.play("Glitter")
-	add_child(m_glitterTimer)
 	m_glitterTimer.start()
 
 
 func stopGlittering():
+	get_node("Sprite/AnimationPlayer").stop()
 	emit_signal("finished")
