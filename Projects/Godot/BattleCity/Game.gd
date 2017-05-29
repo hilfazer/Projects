83899fe92beee565e7d21c3ddf5ceddca73998fd
hilfazer <az13337@gmail.com>
2017-05-29 20:08:12 +0200

    loading next stage

--- Projects/Godot/BattleCity/Game.gd
+++ Projects/Godot/BattleCity/Game.gd
@@ -1,5 +1,6 @@
 extends Node
 
+const MainMenuScn = "res://gui/MainMenu.tscn"
 #stages need to be located in
 const StagesPath = "res://stages/"
 # Stages need to start with string below and have number at the end
@@ -10,6 +11,7 @@ const GameOverScreenDelay = 2
 
 var m_stages = []
 var m_nextStage = 0
+var m_playerCount
 
 
 func _ready():
@@ -25,18 +27,20 @@ func discoverStages():
 		stageNumber += 1
 
 	assert(stages.empty() == false)
-	return stages
+	return stages 
 
 
 func startAGame(playerCount):
-	SceneSwitcher.switchScene( m_stages[0], {playerCount = playerCount} )
+	m_playerCount = playerCount
+	m_nextStage = 0
+	loadStage(0, playerCount)
 	
 	
 func onPlayersWon():
 	var timer = Timer.new()
 	timer.set_wait_time(GameOverScreenDelay)
 	timer.connect("timeout", timer, "queue_free")
-	timer.connect("timeout", self, "gameOver")
+	timer.connect("timeout", self, "stageComplete")
 	self.add_child(timer)
 	timer.start()
 	
@@ -51,4 +55,16 @@ func onPlayersLost():
 
 
 func gameOver():
-	SceneSwitcher.switchScene(SceneSwitcher.m_previousScene)
+	SceneSwitcher.switchScene(MainMenuScn)
+
+
+func stageComplete():
+	m_nextStage += 1
+	if m_nextStage < m_stages.size():
+		loadStage(m_nextStage, m_playerCount)
+	else:
+		gameOver()
+
+
+func loadStage(stageNumber, playerCount):
+	SceneSwitcher.switchScene( m_stages[stageNumber], {playerCount = playerCount} )
