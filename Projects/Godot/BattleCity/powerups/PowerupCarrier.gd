f236f136ed22b71158485792e6a2ecb0c012fd40
hilfazer <az13337@gmail.com>
2017-06-11 20:00:31 +0200

    changing tank color

--- Projects/Godot/BattleCity/powerups/PowerupCarrier.gd
+++ Projects/Godot/BattleCity/powerups/PowerupCarrier.gd
@@ -1,13 +1,18 @@
 extends Node2D
 
 const PowerupFactoryScn = preload("res://powerups/PowerupFactory.tscn")
+const TankGd = preload("res://units/Tank.gd")
 
+const IdxToColor = { 1 : TankGd.ColorOffset.SILVER, 2 : TankGd.ColorOffset.GOLD, \
+	3 : TankGd.ColorOffset.GREEN, 4 : TankGd.ColorOffset.PURPLE }
+	
 var m_tank
 export (String, "", "Helmet", "Star") var m_powerupName = ""
 
 
 func _ready():
 	m_tank = get_parent()
+	get_node("AnimationPlayer").play("changeTankColor")
 
 
 func _exit_tree():
@@ -17,3 +22,8 @@ func _exit_tree():
 		else powerupFactory.makeRandomPowerup()
 	m_tank.m_stage.get_ref().placePowerup( powerup )
 	powerupFactory.free()
+
+
+func changeTankColor(colorIdx):
+	m_tank.setColor( IdxToColor[colorIdx] )
+	
