054c2a43e82058b249d52987bcdd820cfa1aa6c0
hilfazer <az13337@gmail.com>
2017-07-09 13:40:37 +0200

    4 enemy tank types

--- Projects/Godot/BattleCity/actors/EnemyDefinition.gd
+++
@@ -1,4 +0,0 @@
-extends Node
-
-export var shootFrequency = 1.0
-export var spawns = IntArray()
--- Projects/Godot/BattleCity/stages/EnemyDefinition.gd
+++ Projects/Godot/BattleCity/stages/EnemyDefinition.gd
@@ -3,3 +3,4 @@ extends Node
 export var shootFrequency = 1.0
 export var spawnIndices = IntArray()	# empty array for all spawns
 export var spawnTime = 0.0
+export var points = 100
