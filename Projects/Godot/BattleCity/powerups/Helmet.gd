3ec002902c95bc3b634770d944e75a509f71297a
hilfazer <az13337@gmail.com>
2017-08-05 16:41:28 +0200

    working shield

--- Projects/Godot/BattleCity/powerups/Helmet.gd
+++ Projects/Godot/BattleCity/powerups/Helmet.gd
@@ -5,5 +5,3 @@ func execute( stage, tank ):
 	var shieldEffect = ShieldScn.instance()
 	shieldEffect.set_script(ShieldGd)
 	tank.add_child(shieldEffect)
-	
-	
--- Projects/Godot/BattleCity/powerups/Shield.gd
+++ Projects/Godot/BattleCity/powerups/Shield.gd
@@ -1,8 +1,10 @@
 extends Node
 
 
-const m_duration = 6
-var m_durationTimer = Timer.new()
+onready var m_tank   = get_parent()
+const m_duration     = 6
+var m_durationTimer  = Timer.new()
+var m_tankHandleBulletFunRef
 
 
 func _ready():
@@ -11,7 +13,19 @@ func _ready():
 	m_durationTimer.connect("timeout", self, "queue_free")
 	add_child(m_durationTimer)
 	m_durationTimer.start()
+	
+	var handleBulletFunRef = FuncRef.new()
+	handleBulletFunRef.set_function("handleBulletCollision")
+	handleBulletFunRef.set_instance(self)
+
+	m_tankHandleBulletFunRef = m_tank.m_handleBulletFunRef
+	m_tank.m_handleBulletFunRef = handleBulletFunRef
 
 
 func handleBulletCollision(bullet):
 	pass
+	
+	
+func _notification(what):
+	if what == NOTIFICATION_PREDELETE:
+		m_tank.m_handleBulletFunRef = m_tankHandleBulletFunRef
