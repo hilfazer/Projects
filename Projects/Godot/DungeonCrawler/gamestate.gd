0c80f1070a4dd7cd1bae9413ec3bc3b414a58466
hilfazer <az13337@gmail.com>
2017-08-07 19:28:52 +0200

    moved functions around; removed spawn points

--- Projects/Godot/DungeonCrawler/gamestate.gd
+++ Projects/Godot/DungeonCrawler/gamestate.gd
@@ -71,45 +71,6 @@ remote func unregister_player(id):
 	players.erase(id)
 	emit_signal("player_list_changed")
 
-remote func pre_start_game(spawn_points):
-	# Change scene
-	var world = load("res://world.tscn").instance()
-	get_tree().get_root().add_child(world)
-
-	get_tree().get_root().get_node("lobby").hide()
-
-	var player_scene = load("res://player.tscn")
-
-	for p_id in spawn_points:
-		var spawn_pos = world.get_node("spawn_points/" + str(spawn_points[p_id])).position
-		var player = player_scene.instance()
-
-		player.set_name(str(p_id)) # Use unique ID as node name
-		player.position=spawn_pos
-		player.set_network_master(p_id) #set unique id as master
-
-		if (p_id == get_tree().get_network_unique_id()):
-			# If node for this peer id, set name
-			player.set_player_name(player_name)
-		else:
-			# Otherwise set name from peer
-			player.set_player_name(players[p_id])
-
-		world.get_node("players").add_child(player)
-
-	# Set up score
-	world.get_node("score").add_player(get_tree().get_network_unique_id(), player_name)
-	for pn in players:
-		world.get_node("score").add_player(pn, players[pn])
-
-	if (not get_tree().is_network_server()):
-		# Tell server we are ready to start
-		rpc_id(1, "ready_to_start", get_tree().get_network_unique_id())
-	elif players.size() == 0:
-		post_start_game()
-
-remote func post_start_game():
-	get_tree().set_pause(false) # Unpause and unleash the game!
 
 var players_ready = []
 
@@ -142,21 +103,49 @@ func get_player_list():
 func get_player_name():
 	return player_name
 
+
+func _ready():
+	get_tree().connect("network_peer_connected", self, "_player_connected")
+	get_tree().connect("network_peer_disconnected", self,"_player_disconnected")
+	get_tree().connect("connected_to_server", self, "_connected_ok")
+	get_tree().connect("connection_failed", self, "_connected_fail")
+	get_tree().connect("server_disconnected", self, "_server_disconnected")
+	
+	
+
 func begin_game():
 	assert(get_tree().is_network_server())
 
-	# Create a dictionary with peer id and respective spawn points, could be improved by randomizing
-	var spawn_points = {}
-	spawn_points[1] = 0 # Server in spawn point 0
-	var spawn_point_idx = 1
 	for p in players:
-		spawn_points[p] = spawn_point_idx
-		spawn_point_idx += 1
-	# Call to pre-start game with the spawn points
-	for p in players:
-		rpc_id(p, "pre_start_game", spawn_points)
+		rpc_id(p, "pre_start_game")
 
 	pre_start_game(spawn_points)
+
+
+remote func pre_start_game():
+	# Change scene
+	var world = load("res://World.tscn").instance()
+	get_tree().get_root().add_child(world)
+
+	get_tree().get_root().get_node("lobby").hide()
+
+	var player_scene = load("res://player.tscn")
+
+	# Set up score
+	world.get_node("score").add_player(get_tree().get_network_unique_id(), player_name)
+	for pn in players:
+		world.get_node("score").add_player(pn, players[pn])
+
+	if (not get_tree().is_network_server()):
+		# Tell server we are ready to start
+		rpc_id(1, "ready_to_start", get_tree().get_network_unique_id())
+	elif players.size() == 0:
+		post_start_game()
+
+
+remote func post_start_game():
+	get_tree().set_pause(false) # Unpause and unleash the game!
+
 
 func end_game():
 	if (has_node("/root/world")): # Game is in progress
@@ -166,10 +155,3 @@ func end_game():
 	emit_signal("game_ended")
 	players.clear()
 	get_tree().set_network_peer(null) # End networking
-
-func _ready():
-	get_tree().connect("network_peer_connected", self, "_player_connected")
-	get_tree().connect("network_peer_disconnected", self,"_player_disconnected")
-	get_tree().connect("connected_to_server", self, "_connected_ok")
-	get_tree().connect("connection_failed", self, "_connected_fail")
-	get_tree().connect("server_disconnected", self, "_server_disconnected")
