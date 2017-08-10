131b5e8140d626b42f2de6d6c29ab4a33b4fe2f8
hilfazer <az13337@gmail.com>
2017-08-10 16:54:11 +0200

    Join button enables when game ends

--- Projects/Godot/DungeonCrawler/gui/lobby.gd
+++ Projects/Godot/DungeonCrawler/gui/lobby.gd
@@ -54,7 +54,7 @@ func _on_game_ended():
 	get_node("connect").show()
 	get_node("players").hide()
 	get_node("connect/host").disabled=false
-	get_node("connect/join").disabled
+	get_node("connect/join").disabled=false
 
 func _on_game_error(errtxt):
 	get_node("error").dialog_text=errtxt
