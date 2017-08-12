bfb3f6f62195a9c31f9dbbf00cb8bbd3ea7e408b
hilfazer <az13337@gmail.com>
2017-08-12 14:33:41 +0200

    inscreased unit speed

--- Projects/Godot/DungeonCrawler/units/Unit.gd
+++ Projects/Godot/DungeonCrawler/units/Unit.gd
@@ -1,22 +1,23 @@
 extends KinematicBody2D
 
-slave var slave_pos
-var m_movement = Vector2(0,0)
+const Speed = 3
+
+slave var  m_slave_pos
+master var m_movement = Vector2(0,0)
 
 
 func _ready():
 	set_fixed_process(true)
-	slave_pos = self.position
+	m_slave_pos = self.position
 
 
 func _fixed_process(delta):
 	if ( get_tree().is_network_server() ):
-		move( m_movement.normalized() )
-		m_movement = Vector2(0,0)
+		move( m_movement.normalized() * Speed )
 
-		rset_unreliable("slave_pos", self.position)
+		rset_unreliable("m_slave_pos", self.position)
 	else:
-		set_position(slave_pos)
+		set_position(m_slave_pos)
 		
 		
 remote func setMovement( movement ):
