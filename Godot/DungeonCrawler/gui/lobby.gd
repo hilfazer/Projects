79d36bb1889e18cc030f33992856cf4805e26129
hilfazer <az13337@gmail.com>
2017-08-27 12:27:49 +0200

    adjusted code for buttons

--- Godot/DungeonCrawler/gui/lobby.gd
+++ Godot/DungeonCrawler/gui/lobby.gd
@@ -34,32 +34,32 @@ func onJoinPressed():
 		return
 
 	get_node("connect/error_label").text=""
-	get_node("connect/host").disabled=true
-	get_node("connect/join").disabled=true
+	get_node("connect/buttons/host").disabled=true
+	get_node("connect/buttons/join").disabled=true
 
 	var name = get_node("connect/name").text
 	gamestate.joinGame(ip, name)
 	# refreshLobby() gets called by the playerListChanged signal
 
 func onConnectionSuccess():
-	get_node("connect").hide()
+	pass
 
 func onConnectionFailed():
-	get_node("connect/host").disabled=false
-	get_node("connect/join").disabled=false
+	get_node("connect/buttons/host").disabled=false
+	get_node("connect/buttons/join").disabled=false
 	get_node("connect/error_label").set_text("Connection failed.")
 
 func onGameEnded():
 	show()
 	get_node("connect").show()
-	get_node("connect/host").disabled=false
-	get_node("connect/join").disabled=false
+	get_node("connect/buttons/host").disabled=false
+	get_node("connect/buttons/join").disabled=false
 
 func onGameError(errtxt):
 	get_node("error").dialog_text=errtxt
 	get_node("error").popup_centered_minsize()
-	get_node("connect/host").disabled=false
-	get_node("connect/join").disabled=false
+	get_node("connect//buttons/host").disabled=false
+	get_node("connect/buttons/join").disabled=false
 
 func refreshLobby():
 	var players = gamestate.m_players
@@ -70,20 +70,20 @@ func refreshLobby():
 		playerString += " (You)" if p == get_tree().get_network_unique_id() else ""
 		get_node("players/list").add_item(playerString)
 
-	get_node("players/start").disabled=not get_tree().is_network_server()
+	get_node("connect/buttons/start").disabled=not get_tree().is_network_server()
 
 func onStartPressed():
 	gamestate.beginGame()
-	get_node("players/stop").disabled= false
+	get_node("connect/buttons/stop").disabled= false
 
 
 func onStopPressed():
 	gamestate.endGame()
-	get_node("players/stop").disabled= true
+	get_node("connect/buttons/stop").disabled= true
 	
 	
 func onNetworkPeerChanged():
-	get_node("players/start").disabled= \
+	get_node("connect/buttons/start").disabled= \
 		not (get_tree().has_network_peer() and get_tree().is_network_server() )
-	get_node("players/stop").disabled=not get_tree().has_network_peer()
+	get_node("connect/buttons/stop").disabled=not get_tree().has_network_peer()
 	
