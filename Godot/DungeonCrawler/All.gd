dd3e0791586f35bbfe5a65083af523f85fac1112
hilfazer <az13337@gmail.com>
2017-09-09 13:00:20 +0200

    Revert "'Units' node's type changed to Node2D"
    This reverts commit bf5edad739dbc3631ae6792cce66713314fdf4d4.

--- Godot/DungeonCrawler/All.gd
+++ Godot/DungeonCrawler/All.gd
@@ -2,7 +2,7 @@ extends Node
 
 
 func _ready():
-	gamestate.m_levelParentNodePath = get_node("LevelContainer").get_path()
+	gamestate.m_levelParentNodePath = get_node("LevelContainer/Viewport").get_path()
 	
 	$"ChooseModuleDialog/FileDialog".connect( "visibility_changed",
 		self, "onDialogVisibilityChanged", [$"ChooseModuleDialog/FileDialog"] )
