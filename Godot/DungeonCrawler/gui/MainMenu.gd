644d6e5283865efd0702689a522f1ba0696c8125
hilfazer <az13337@gmail.com>
2017-11-25 13:39:18 +0100

    joining server

--- Godot/DungeonCrawler/gui/MainMenu.gd
+++ Godot/DungeonCrawler/gui/MainMenu.gd
@@ -7,9 +7,19 @@ func _ready():
 	Connector.connectMainMenu(self)
 
 
-func onNewGamePressed():
+func newGame():
 	var params = {}
 	params["playerName"] = get_node("Connect/Name").text
 	params["ip"] = get_node("Connect/Ip").text
 	params["host"] = true
+
+	SceneSwitcher.switchScene(NewGameScnPath, params)
+
+
+func joinGame():
+	var params = {}
+	params["playerName"] = get_node("Connect/Name").text
+	params["ip"] = get_node("Connect/Ip").text
+	params["host"] = false
+
 	SceneSwitcher.switchScene(NewGameScnPath, params)
