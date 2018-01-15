d474a7ff19798c392fe5d394097efa1a3a29414d
hilfazer <az13337@gmail.com>
2018-01-15 18:56:23 +0100

    save game dialog

--- Godot/DungeonCrawler/gui/GameMenu.gd
+++ Godot/DungeonCrawler/gui/GameMenu.gd
@@ -1,9 +1,17 @@
 extends Control
 
 
+const SaveGameDirectory = "res://save"
+
+
 func onResumePressed():
 	get_parent().deleteGameMenu()
 
 
 func onQuitPressed():
 	get_parent().emit_signal("quitGameRequested")
+
+
+func onSavePressed():
+	get_node("SaveGameDialog").set_current_dir(SaveGameDirectory)
+	get_node("SaveGameDialog").show()
