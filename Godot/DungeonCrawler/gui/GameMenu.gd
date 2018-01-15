ca22def99d50835742189bab82f9ba9b2fc36f67
hilfazer <az13337@gmail.com>
2018-01-15 20:16:21 +0100

    adding .sav extension to save files if necessary

--- Godot/DungeonCrawler/gui/GameMenu.gd
+++ Godot/DungeonCrawler/gui/GameMenu.gd
@@ -2,6 +2,7 @@ extends Control
 
 
 const SaveGameDirectory = "res://save"
+const SaveFileExtension = "sav"
 
 
 func onResumePressed():
@@ -15,3 +16,11 @@ func onQuitPressed():
 func onSavePressed():
 	get_node("SaveGameDialog").set_current_dir(SaveGameDirectory)
 	get_node("SaveGameDialog").show()
+
+
+func saveToFile( filename ):
+	var filenameWithExtension = filename
+	if filenameWithExtension.get_extension() != SaveFileExtension:
+		filenameWithExtension = filenameWithExtension + "." + SaveFileExtension
+
+	get_parent().emit_signal("saveToFileRequested", filenameWithExtension)
