6a0e513859128ce8d7f113580026cb097b68b0b1
hilfazer <az13337@gmail.com>
2018-08-02 13:55:23 +0200

    TODO update

--- Godot/DungeonCrawler/TODO.gd
+++ Godot/DungeonCrawler/TODO.gd
@@ -2,15 +2,12 @@
 
 * lobby for live game where units are assigned to players
 
-* transition between levels
-
 * merging in saving system
-
-* debugger errors when loading a game with clients connected
 
 * synchronize destruction of levels and units
 
-* check why level transition does not work for more than 2 players
-	and why clients loose control over their units
+* check why clients loose control over their units
+
+* player agents that control multiple units
 
 """
