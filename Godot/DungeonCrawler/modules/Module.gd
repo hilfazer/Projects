38b00595828d2b984969b11c064f77ecedf0915b
hilfazer <az13337@gmail.com>
2018-08-10 10:50:57 +0200

    moved functions from Module1.gd to Module.gd

--- Godot/DungeonCrawler/modules/Module.gd
+++ Godot/DungeonCrawler/modules/Module.gd
@@ -1,26 +1,39 @@
 # abstract class for new or saved module
 extends Node
 
+# override those constants in subclass
+const UnitMax = null
+const Units = []
+const LevelNamesToFilenames = {}
+const LevelConnections = {}
+
+
+func getPlayerUnitMax():
+	return UnitMax
+
 
 func getUnitsForCreation():
-	assert(false)
+	return Units
+
+
+func getStartingLevelFilenameAndEntrance():
+	return [ LevelNamesToFilenames["Level1"], "Entrance" ]
+
+
+func getLevelFilename( levelName ):
+	assert( LevelNamesToFilenames.has(levelName) )
+	return LevelNamesToFilenames[levelName]
+
+
+func getTargetLevelFilenameAndEntrance( sourceLevelName, entrance ):
+	assert( LevelNamesToFilenames.has(sourceLevelName) )
+	if not LevelConnections.has( [sourceLevelName, entrance] ):
+		return null
+
+	var name_entrance = LevelConnections[[sourceLevelName, entrance]]
+
+	return [ getLevelFilename( name_entrance[0] ), name_entrance[1] ]
 
 
 func getExistingPlayerUnits():
 	return []       # default for new modules
-
-
-func getStartingLevelFilenameAndEntrance():
-	assert(false)
-	
-	
-func getLevelFilename( levelName ):
-	assert(false)
-	
-	
-func getTargetLevelFilenameAndEntrance( sourceLevelName, entrance ):
-	assert(false)
-
-
-func getPlayerUnitMax():
-	assert(false)
--- Godot/DungeonCrawler/modules/Module1.gd
+++ Godot/DungeonCrawler/modules/Module1.gd
@@ -12,30 +12,3 @@ const LevelConnections = {
 	["Level1", "ToLevel2"] : ["Level2", "Entrance"],
 	["Level2", "ToLevel1"] : ["Level1", "ToLevel2"]
 }
-
-
-func getPlayerUnitMax():
-	return UnitMax
-
-
-func getUnitsForCreation():
-	return Units
-
-
-func getStartingLevelFilenameAndEntrance():
-	return [ LevelNamesToFilenames["Level1"], "Entrance" ]
-
-
-func getLevelFilename( levelName ):
-	assert( LevelNamesToFilenames.has(levelName) )
-	return LevelNamesToFilenames[levelName]
-
-
-func getTargetLevelFilenameAndEntrance( sourceLevelName, entrance ):
-	assert( LevelNamesToFilenames.has(sourceLevelName) )
-	if not LevelConnections.has( [sourceLevelName, entrance] ):
-		return null
-
-	var name_entrance = LevelConnections[[sourceLevelName, entrance]]
-
-	return [ getLevelFilename( name_entrance[0] ), name_entrance[1] ]
