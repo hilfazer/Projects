4181910060da578c0ce57c7e10348db658ac9a0b
hilfazer <az13337@gmail.com>
2018-08-11 12:47:20 +0200

    files with different module

---
+++ Godot/DungeonCrawler/modules/Module2.gd
@@ -0,0 +1,14 @@
+extends Reference
+
+const UnitMax = 4
+const Units = ["res://units/Dwarf.tscn", "res://units/Gnome.tscn", "res://units/Wizard.tscn"]
+
+const LevelNamesToFilenames = {
+	"Level1" : "res://levels/Level1.tscn",
+	"Level2" : "res://levels/Level2.tscn"
+	}
+
+const LevelConnections = {
+	["Level1", "ToLevel2"] : ["Level2", "Entrance"],
+	["Level2", "ToLevel1"] : ["Level1", "ToLevel2"]
+}
