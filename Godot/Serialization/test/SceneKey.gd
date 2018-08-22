ec3515e9fceee5cb503c11953f59684e6b75b8fa
hilfazer <az13337@gmail.com>
2018-08-22 20:36:46 +0200

    proper dictionary keys

--- Godot/Serialization/test/SceneKey.gd
+++ Godot/Serialization/test/SceneKey.gd
@@ -5,9 +5,9 @@ export var b = 1
 
 
 func serialize():
-	return { a : a, b : b, "SCENE" : filename }
+	return { '0' : a, '1' : b, "SCENE" : filename }
 
 
 func deserialize( data ):
-	a = data[a]
-	b = data[b]
+	a = data['0']
+	b = data['1']
