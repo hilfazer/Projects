9418afb6980e7732e63fd42991e53c10c916c5f9
hilfazer <az13337@gmail.com>
2018-09-08 16:17:41 +0200

    added 'sceneInstanced()' signal

---
+++ Godot/SceneSwitcher/Global.gd
@@ -0,0 +1,19 @@
+extends Node
+
+
+func _init():
+	SceneSwitcher.connect("sceneInstanced", self, "onInstanced")
+	SceneSwitcher.connect("currentSceneChanged", self, "onCurrentChanged")
+
+
+func onInstanced( scene ):
+	print( "Scene %s instanced" % scene.filename )
+	scene.connect("tree_entered", self, "onEntered", [scene], CONNECT_ONESHOT )
+
+
+func onEntered( scene ):
+	print( "Scene %s entering tree" % scene.filename )
+
+
+func onCurrentChanged():
+	print( "Scene %s is a current scene" % get_tree().current_scene.filename )
