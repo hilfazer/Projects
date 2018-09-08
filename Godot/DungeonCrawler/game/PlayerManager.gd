35ecaf8c8cd2b1d99d97ff6853c767e13b0f3903
hilfazer <az13337@gmail.com>
2018-09-08 20:00:35 +0200

    refactored unregistering a command

--- Godot/DungeonCrawler/game/PlayerManager.gd
+++ Godot/DungeonCrawler/game/PlayerManager.gd
@@ -21,10 +21,6 @@ func _enter_tree():
 	_registerCommands()
 
 
-func _exit_tree():
-	_unregisterCommands()
-
-
 func _ready():
 	Connector.connectPlayerManager( self )
 	onClientListChanged( Network.m_clients )
@@ -187,13 +183,11 @@ func _registerCommands():
 	if not is_network_master():
 		return
 
-	Console.register('unassignUnits', {
+	var unassignUnits = "unassignUnits"
+	Console.register(unassignUnits, {
 		'description' : "unassigns all player units",
 		'target' : [self, '_unassignAllUnits']
 	} )
-
-
-func _unregisterCommands():
-	Console.deregister('unassignUnits')
+	connect( "tree_exiting", Console, "deregister", [unassignUnits] )
 
 
