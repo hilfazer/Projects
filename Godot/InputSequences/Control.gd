2b132b2dd264a408de6bcb8e76a265c01b7d1f6a
hilfazer <az13337@gmail.com>
2018-12-19 19:04:16 +0100

    adding and removing sequences

--- Godot/InputSequences/Control.gd
+++ Godot/InputSequences/Control.gd
@@ -1,7 +1,7 @@
 extends Control
 
 
-const sequences = { 
+const m_sequences = { 
 	1 : ["ui_up", "ui_up", "ui_up"],
 }
 
@@ -10,15 +10,16 @@ func _ready():
 	
 	$SequenceDetector.connect("sequenceDetected", self, "onSequenceDetected")
 	
-	for id in sequences:
-		var added = $SequenceDetector.addSequence( id, sequences[id] )
-		if added:
-			$AvailableSequences.add_item( str(sequences[id]) )
+	for id in m_sequences:
+		var added = $SequenceDetector.addSequence( id, m_sequences[id] )
+		if added == OK:
+			$AvailableSequences.add_item( str(m_sequences[id]) )
+	
 	
 	pass
 
 
 func onSequenceDetected( id : int ):
-	$PerformedSequences.add_item( str( sequences ) )
+	$PerformedSequences.add_item( str( m_sequences ) )
 	
 	
--- Godot/InputSequences/SequenceDetector.gd
+++ Godot/InputSequences/SequenceDetector.gd
@@ -15,10 +15,17 @@ func _input(event):
 
 
 func addSequence( id : int, sequence : Array ):
-	return true
+	if m_sequences.has( id ):
+		return "Sequence ID %d already exists" % id
+		
+	for seq in m_sequences.values():
+		if seq == sequence:
+			return "Sequence %s already exists" % str(sequence)
+	
+	m_sequences[id] = sequence
 	
 	
 func removeSequence( id : int ):
-	pass
+	m_sequences.erase( id )
 	
 	
