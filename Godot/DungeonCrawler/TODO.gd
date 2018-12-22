11493bd0c0a09bd121ec5f2bf8780588e3587654
hilfazer <az13337@gmail.com>
2018-12-22 15:13:04 +0100

    TODO update

--- Godot/DungeonCrawler/TODO.gd
+++ Godot/DungeonCrawler/TODO.gd
@@ -1,19 +1,17 @@
 """
 
-* lobby for live game where units are assigned to players
-
 * synchronize destruction of levels and units
-
-* grid movement
-
-* spawning more units than can fit on level entrance
-
-* transfer action release events to PlayerAgent
 
 * fix assigning units to client's agents after level change
 
 * fix clients spawning more units than they should
 
-* debug autoload
+* transfer action release events to PlayerAgent
+
+* lobby for live game where units are assigned to players
+
+* grid movement
+
+* spawning more units than can fit on level entrance
 
 """
