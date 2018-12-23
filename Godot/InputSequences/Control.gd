4193120f9739ce0309ef3101be2626c39a30d605
hilfazer <az13337@gmail.com>
2018-12-23 11:34:31 +0100

    connected "CheckBox" to "setConsumingInput"

--- Godot/InputSequences/Control.gd
+++ Godot/InputSequences/Control.gd
@@ -1,13 +1,14 @@
 extends Control
 
 
-const m_sequences = { 
+const m_sequences = {
 	1 : ["ui_up", "ui_up", "ui_up"],
-	2 : ["ui_up", "ui_up"],         # it won't be detected since it's a subsequence of first item
-									# however it will be detected by SequenceDetector2.gd
+	2 : ["ui_up", "ui_up"],         # it won't be detected by SequenceDetector.gd
+									# but it will be by SequenceDetector2.gd
 	3 : ["ui_down"],
 	4 : ["ui_down", "ui_up", "ui_down"],
 	5 : ["ui_right", "ui_right"],
+	6 : ["ui_right", "ui_left"],
 	8 : ["ui_up", "ui_up", "ui_up"],
 	9 : [],
 }
@@ -19,9 +20,26 @@ func _ready():
 	var detector = $"SequenceDetector"
 	detector.setConsumingInput( $"CheckBox".pressed )
 	detector.connect("sequenceDetected", self, "onSequenceDetected")
+	$"CheckBox".connect("toggled", detector, "setConsumingInput")
+
+
+	# SequenceDetector2.gd
+
+	if detector.has_method( "addSequences" ):
+		var discarded : Dictionary = detector.addSequences( m_sequences )
+		for id in m_sequences:
+			if id in discarded:
+				print( discarded[id], " ", id, " ", m_sequences[id] )
+			else:
+				$"AvailableSequences".add_item( str(m_sequences[id]) )
+
+	if detector.has_method( "addActions" ):
+		detector.addActions( m_actions )
+		detector.removeActions( ["ui_select"] )
+
 
 	# SequenceDetector.gd
-	
+
 	if detector.has_method("addSequence"):
 		for id in m_sequences:
 			var result = detector.addSequence( id, m_sequences[id] )
@@ -31,21 +49,6 @@ func _ready():
 				print( result )
 
 
-	# SequenceDetector2.gd
-	
-	if detector.has_method( "addSequences" ):
-		var discarded : Dictionary = detector.addSequences( m_sequences )
-		for id in m_sequences:
-			if id in discarded:
-				print( discarded[id], " ", id, " ", m_sequences[id] )
-			else:
-				$"AvailableSequences".add_item( str(m_sequences[id]) )
-		
-	if detector.has_method( "addActions" ):
-		detector.addActions( m_actions )
-		detector.removeActions( ["ui_select"] )
-		
-
 func _input(event):
 	if event is InputEventKey and event.pressed:
 		print( "pressed key scancode ", event.scancode )
@@ -53,5 +56,4 @@ func _input(event):
 
 func onSequenceDetected( id : int ):
 	$"PerformedSequences".add_item( str( m_sequences[id] ) )
-	
-	
+
