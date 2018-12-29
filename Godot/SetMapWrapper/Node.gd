9f39226f44ddd8608fc1279ef0b225cd49de6e4c
hilfazer <az13337@gmail.com>
2018-12-29 22:52:25 +0100

    SetWrapper's tests

--- Godot/SetMapWrapper/Node.gd
+++ Godot/SetMapWrapper/Node.gd
@@ -3,23 +3,28 @@ extends Node
 
 const SetWrapperGd = preload("res://SetWrapper.gd")
 
-var set = [1,2,3]
-var setWrapper = SetWrapperGd.new( set )
+var setWrapper = SetWrapperGd.new( [1,2,3] )   setget , getSet
 
 
 func _ready():
-	print( set )
+	print( setWrapper.m_array )
 	setWrapper.connect("changed", self, "printChanged")
 
 	setWrapper.add( [4] )
-	assert( set == [1,2,3,4] )
-	setWrapper.remove( [4] )
+	assert( setWrapper.m_array == [1,2,3,4] )
+	setWrapper.remove( [3] )
+	assert( setWrapper.m_array == [1,2,4] )
 	setWrapper.remove( [8] )
+	assert( setWrapper.m_array == [1,2,4] )
 	setWrapper.add( [7] )
+	assert( setWrapper.m_array == [1,2,4,7] )
 	setWrapper.reset( [4,3,6] )
-	assert( set == [4,3,6] )
+	assert( setWrapper.m_array == [4,3,6] )
 
 
 func printChanged( reference ):
 	print( reference )
 	
+	
+func getSet():
+	return setWrapper.m_array
