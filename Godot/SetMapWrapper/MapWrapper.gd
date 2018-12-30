7c1248e588aeb4c40d5a923b8b2cd694fb987a40
hilfazer <az13337@gmail.com>
2018-12-30 08:42:59 +0100

    MapWrapper.gd + tests

---
+++ Godot/SetMapWrapper/MapWrapper.gd
@@ -0,0 +1,56 @@
+extends Reference
+
+var m_dict = {}       setget deleted
+
+
+signal changed( dict )
+
+
+func deleted(_a):
+	assert(false)
+
+
+func _init( dict = [] ):
+	m_dict = dict
+
+
+func reset( dict ):
+	if m_dict == dict:
+		return
+
+	m_dict = dict
+	emit_signal("changed", m_dict )
+
+# doesn't alter values of existing keys
+func add( dict ):
+	var size = m_dict.size()
+	for x in dict:
+		if not x in m_dict:
+			m_dict[x] = dict[x]
+
+	if m_dict.size() > size:
+		emit_signal("changed", m_dict )
+
+# alters values of existing keys, can add new keys
+func replace( dict ):
+	var changed = false
+	for x in dict:
+		if not x in m_dict or _notEqual(m_dict[x], dict[x]):
+			m_dict[x] = dict[x]
+			changed = true
+
+	if changed:
+		emit_signal("changed", m_dict )
+
+
+func remove( dict ):
+	var size = m_dict.size()
+	for x in dict:
+		m_dict.erase( x )
+
+	if m_dict.size() < size:
+		emit_signal("changed", m_dict )
+
+
+static func _notEqual( a, b ):
+	return typeof(a) != typeof(b) or a != b
--- Godot/SetMapWrapper/Node.gd
+++ Godot/SetMapWrapper/Node.gd
@@ -2,13 +2,18 @@ extends Node
 
 
 const SetWrapperGd = preload("res://SetWrapper.gd")
+const MapWrapperGd = preload("res://MapWrapper.gd")
 
 var setWrapper = SetWrapperGd.new( [1,2,3] )   setget , getSet
+var setChanges = 0
+
+var mapWrapper = MapWrapperGd.new( {1:2, 2:3} )   setget , getMap
+var mapChanges = 0
 
 
 func _ready():
 	print( setWrapper.m_array )
-	setWrapper.connect("changed", self, "printChanged")
+	setWrapper.connect("changed", self, "printSetChanged")
 
 	setWrapper.add( [4] )
 	assert( setWrapper.m_array == [1,2,3,4] )
@@ -20,11 +25,37 @@ func _ready():
 	assert( setWrapper.m_array == [1,2,4,7] )
 	setWrapper.reset( [4,3,6] )
 	assert( setWrapper.m_array == [4,3,6] )
+	assert( setChanges == 4 )
+	
+	print(mapWrapper.m_dict )
+	mapWrapper.connect("changed", self, "printMapChanged")
+	
+	mapWrapper.add( {'s':1, 8:'e'} )
+	assert( mapWrapper.m_dict.hash() == {1:2, 2:3,'s':1, 8:'e'}.hash() )
+	mapWrapper.add( {'s':'p', 8:5} )
+	assert( mapWrapper.m_dict.hash() == {1:2, 2:3,'s':1, 8:'e'}.hash() )
+	mapWrapper.reset( {1:2, 2:3, 3:4} )
+	assert( mapWrapper.m_dict.hash() == {1:2, 2:3, 3:4}.hash() )
+	mapWrapper.remove( {2:3} )
+	assert( mapWrapper.m_dict.hash() == {1:2, 3:4}.hash() )
+	mapWrapper.remove( {2:3} )
+	assert( mapWrapper.m_dict.hash() == {1:2, 3:4}.hash() )
+	mapWrapper.replace( {1:'o', 6:4} )
+	assert( mapWrapper.m_dict.hash() == {1:'o', 3:4, 6:4}.hash() )
+	assert( mapChanges == 4 )
 
 
-func printChanged( reference ):
+func printSetChanged( reference ):
 	print( reference )
+	setChanges += 1
+	
+func printMapChanged( reference ):
+	print( reference )
+	mapChanges += 1
 	
 	
 func getSet():
 	return setWrapper.m_array
+
+func getMap():
+	return mapWrapper.m_dict
