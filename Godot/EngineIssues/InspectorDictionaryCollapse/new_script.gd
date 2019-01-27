bfc604e7d715ca808c336baa29cdabe67a653f05
hilfazer <az13337@gmail.com>
2019-01-27 13:09:32 +0100

    can't see 3rd level of nesting for dictionaries/arrays in Inspector

---
+++ Godot/EngineIssues/InspectorDictionaryCollapse/new_script.gd
@@ -0,0 +1,16 @@
+extends Node
+
+var dict = {
+	'dict1' : {
+		'dict2' : { 1 : 'cant see' },
+		'array2' : ['cant see'],
+		},
+	'array1' : [
+		3,
+		['cant see']
+		],
+	}
+
+
+func _ready():
+	pass # put a breakpoint here
