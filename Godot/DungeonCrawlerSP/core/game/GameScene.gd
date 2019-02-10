4ce67cee4f664317d2a9200762853f6f3edc9a02
hilfazer <az13337@gmail.com>
2019-02-10 11:30:24 +0100

    State in GameScene.gd

--- Godot/DungeonCrawlerSP/core/game/GameScene.gd
+++ Godot/DungeonCrawlerSP/core/game/GameScene.gd
@@ -2,10 +2,40 @@ extends Node
 
 
 enum Params { Module, PlayerUnitsData }
+enum State { Initial, Creating, Running, Finished }
 
+var m_state : int = State.Initial      setget deleted # _changeState
 
 signal gameFinished()
 
 
+func deleted(_a):
+	assert(false)
+
+
 func finish():
-	call_deferred( "emit_signal", "gameFinished" )
+	_changeState( State.Finished )
+
+
+func setPaused( enabled : bool ):
+	get_tree().paused = enabled
+	Debug.updateVariable( "Pause", "Yes" if get_tree().paused else "No" )
+
+
+func _changeState( state : int ):
+	assert( m_state != State.Finished )
+
+	if state == m_state:
+		Debug.warn(self, "changing to same state")
+		return
+
+	if state == State.Finished:
+		call_deferred( "emit_signal", "gameFinished" )
+
+	elif state == State.Running:
+		setPaused(false)
+
+	elif state == State.Creating:
+		setPaused(true)
+
+	m_state = state
