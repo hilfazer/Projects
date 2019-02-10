5ef610364f71fd64afc3232a8a132e07bb069842
hilfazer <az13337@gmail.com>
2019-02-10 10:22:36 +0100

    'console_toggle' action

--- Godot/DungeonCrawlerSP/debug/DebugCommands.gd
+++ Godot/DungeonCrawlerSP/debug/DebugCommands.gd
@@ -2,11 +2,6 @@ extends "res://debug/CommandHolder.gd"
 
 
 func _registerCommands():
-	registerCommand( "setRpcLog", {
-		'description' : "sets logging RPCs",
-		'args':[ ['logEnabled', TYPE_BOOL] ],
-		'target' : [self, "setRpcLog"]
-	} )
 	registerCommand( "setLogLevel", {
 		'description' : "sets debug logging level",
 		'args':[ ['level', TYPE_INT] ],
