ada937311361833b60d827eb54881e1265d76ed7
hilfazer <az13337@gmail.com>
2019-02-11 20:57:54 +0100

    player agent

--- Godot/DungeonCrawlerSP/TODO.gd
+++ Godot/DungeonCrawlerSP/TODO.gd
@@ -2,10 +2,10 @@
 
 * saves in usr:// directory
 
-* player agent
-
 * grid movement
 
 * unit selection
+
+* enemy agent
 
 """
--- Godot/DungeonCrawlerSP/core/game/PlayerManager.gd
+++ Godot/DungeonCrawlerSP/core/game/PlayerManager.gd
@@ -1,6 +1,7 @@
 extends Node
 
 const PlayerUnitGd           = preload("./PlayerUnit.gd")
+const PlayerAgentGd          = preload("res://core/PlayerAgent.gd")
 
 var m_playerUnits : Array = []         setget  setPlayerUnits
 
@@ -10,6 +11,8 @@ var m_playerUnits : Array = []         setget  setPlayerUnits
 func setPlayerUnits( units : Array ):
 	for unit in units:
 		assert( unit is PlayerUnitGd )
+		unit.m_unitNode_.add_child( PlayerAgentGd.new() )
+
 	m_playerUnits = []
 	m_playerUnits = units
 
