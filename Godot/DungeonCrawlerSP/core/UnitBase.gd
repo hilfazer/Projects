262a41274d123460a5e600101b82c7d8d1d19023
hilfazer <az13337@gmail.com>
2019-03-24 15:30:29 +0100

    added '_speed' variable to UnitBase that affects animation length

--- Godot/DungeonCrawlerSP/core/UnitBase.gd
+++ Godot/DungeonCrawlerSP/core/UnitBase.gd
@@ -5,9 +5,13 @@ const Speed = 3
 const UnitNameLabel = "Name"
 
 export var _cellSize := Vector2(16, 16)
+export (float) var _speed = 1.0
 
 var _isMoving := false
-onready var _nameLabel = $"Name"
+onready var _nameLabel := $"Name"
+onready var _animationPlayer := $"Pivot/AnimationPlayer"
+onready var _tween := $"Pivot/Tween"
+onready var _pivot := $"Pivot"
 
 
 signal predelete()
@@ -17,6 +21,10 @@ signal moved( direction ) # Vector2
 
 func _init():
 	Debug.updateVariable("Unit count", +1, true)
+
+
+func _ready():
+	_animationPlayer.playback_speed = _speed
 
 
 func _notification(what):
@@ -35,18 +43,20 @@ func moveInDirection( direction : Vector2 ):
 		return 3
 
 	_isMoving = true
-	$'Pivot/AnimationPlayer'.play("move")
-	$'Pivot/Tween'.interpolate_property(
-		$'Pivot', "position", - direction * _cellSize, Vector2(), \
-		$'Pivot/AnimationPlayer'.current_animation_length, \
+	_animationPlayer.play("move")
+	var duration : float = _animationPlayer.current_animation_length / \
+		_animationPlayer.playback_speed
+	_tween.interpolate_property(
+		_pivot, "position", - direction * _cellSize, Vector2(), \
+		duration, \
 		Tween.TRANS_LINEAR, Tween.EASE_IN
 		)
 	position += direction * _cellSize
 	emit_signal("changedPosition")
 
-	$'Pivot/Tween'.start()
+	_tween.start()
 
-	yield( $'Pivot/AnimationPlayer', "animation_finished" )
+	yield( _animationPlayer, "animation_finished" )
 	_isMoving = false
 	emit_signal("moved", direction)
 	return OK
@@ -59,11 +69,12 @@ func setNameLabel( newName ):
 func serialize():
 	var dict := { x = position.x, y = position.y }
 
-	if $'Pivot'.position:
-		dict['pivot_x'] = $'Pivot'.position.x
-		dict['pivot_y'] = $'Pivot'.position.y
-		dict['animationLeft'] = $'Pivot/AnimationPlayer'.current_animation_length - \
-			$'Pivot/AnimationPlayer'.current_animation_position
+	if _pivot.position:
+		dict['pivot_x'] = _pivot.position.x
+		dict['pivot_y'] = _pivot.position.y
+		dict['animationLeft'] = _animationPlayer.current_animation_length * \
+			_animationPlayer.playback_speed - \
+			_animationPlayer.current_animation_position
 	return dict
 
 
@@ -76,18 +87,18 @@ func deserialize( saveDict ):
 
 func _animateMovement( from : Vector2, time : float ):
 	_isMoving = true
-	var speed = $'Pivot/AnimationPlayer'.get_animation("move").length / time
+	var speed = _animationPlayer.get_animation("move").length / time
 
-	$'Pivot/AnimationPlayer'.play("move", -1, speed)
-	$'Pivot/Tween'.interpolate_property(
-		$'Pivot', "position", from, Vector2(), \
+	_animationPlayer.play("move", -1, speed)
+	_tween.interpolate_property(
+		_pivot, "position", from, Vector2(), \
 		time, \
 		Tween.TRANS_LINEAR, Tween.EASE_IN
 		)
 
-	$'Pivot/Tween'.start()
+	_tween.start()
 
-	yield( $'Pivot/AnimationPlayer', "animation_finished" )
+	yield( _animationPlayer, "animation_finished" )
 	_isMoving = false
 
 
