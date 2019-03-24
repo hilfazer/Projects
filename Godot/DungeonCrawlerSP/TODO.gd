73a7d7c2db75f57cce4f102f536fe3d2bcfb05c2
hilfazer <az13337@gmail.com>
2019-03-24 16:30:07 +0100

    TODO update

--- Godot/DungeonCrawlerSP/TODO.gd
+++ Godot/DungeonCrawlerSP/TODO.gd
@@ -1,14 +1,13 @@
 """
 
-* remove preloading Utility.gd
-
-* remove Viewport for level
 
 * don't move units when console is opened
 
 * level transition
 
 * remove units from fog vision
+
+* remove Viewport for level
 
 * saves in usr:// directory
 
