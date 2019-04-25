0936a639b969287914af273ad3934b1c1c132bb0
hilfazer <az13337@gmail.com>
2019-04-25 14:47:50 +0200

    test no matching deserialize()

--- Godot/Serialization/test/TestNoMatchingDeserialize.gd
+++ Godot/Serialization/test/TestNoMatchingDeserialize.gd
@@ -1,17 +1,27 @@
 extends "./TestBase.gd"
 
-var nodes := []
+var nodesNoMatchingDeserialize := []
 
 
 func _runTest():
 	var results := SerializerGd.serializeTest( self )
-	nodes = results.getNodesNoMatchingDeserialize()
+	nodesNoMatchingDeserialize = results.getNodesNoMatchingDeserialize()
+
+	var serializer = SerializerGd.new()
+	var savedData = SerializerGd.serialize( self )
+
+	serializer.add( _testName, savedData )
+	serializer.saveToFile( _saveFilename )
+
+	if OK == serializer.loadFromFile( _saveFilename ):
+		var loadedData = serializer.getValue( _testName )
+		SerializerGd.deserialize( loadedData, get_parent() )
 
 
 func _validate() -> int:
-	var passed = nodes.size() == 2 and \
-		nodes.find( self ) != -1 and \
-		nodes.find( $"Control" ) != -1
+	var passed = nodesNoMatchingDeserialize.size() == 2 and \
+		nodesNoMatchingDeserialize.find( self ) != -1 and \
+		nodesNoMatchingDeserialize.find( $"Control" ) != -1
 	return 0 if passed else 1
 
 
