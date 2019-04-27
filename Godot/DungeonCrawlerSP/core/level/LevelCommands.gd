c2122a3e2c030fc2aeb96c139a21ac2873dc9b3e
hilfazer <az13337@gmail.com>
2019-04-27 21:24:17 +0200

    setGroundTile command

--- Godot/DungeonCrawlerSP/core/level/LevelCommands.gd
+++ Godot/DungeonCrawlerSP/core/level/LevelCommands.gd
@@ -6,15 +6,17 @@ func _ready():
 
 
 func _registerCommands():
-	registerCommand( "setTile",
+	registerCommand( "setGroundTile",
 	{
 		'description' : "unloads current level",
 		'args':[ ['tileName', TYPE_STRING], ['x', TYPE_INT], ['y', TYPE_INT] ],
-		'target' : [self, "setTile"]
+		'target' : [self, "setGroundTile"]
 	} )
 
 
-func setTile( tileName, x, y ):
+func setGroundTile( tileName, x, y ):
 	if -1 !=  $"../Ground".get_tileset().find_tile_by_name( tileName ):
 		$"../Ground".setTile(tileName, x, y)
-
+	else:
+		Console.Log.log(
+			"No ground tile named '%s' " % [tileName], Console.Log.TYPE.WARNING )
