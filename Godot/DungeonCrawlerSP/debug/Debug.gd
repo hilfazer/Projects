847377dd33dfcb1eed816a720155b66f9640e88a
hilfazer <az13337@gmail.com>
2019-04-30 11:43:55 +0200

    'ignored' folder

--- Godot/DungeonCrawlerSP/debug/Debug.gd
+++ Godot/DungeonCrawlerSP/debug/Debug.gd
@@ -5,7 +5,7 @@ const LoggerBaseGd           = preload("res://debug/LoggerBase.gd")
 const ConsoleLoggerGd        = preload("res://debug/ConsoleLogger.gd")
 const FileLoggerGd           = preload("res://debug/FileLogger.gd")
 
-const LogFilename = "res://logfile.log"
+const LogFilename = "res://ignored/logfile.log"
 
 var _debugWindow : CanvasLayer         setget deleted
 var _variables := {}                   setget deleted
