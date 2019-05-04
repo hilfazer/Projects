0fe4a8493c8f2540e86ea3066bca10344deed748
hilfazer <az13337@gmail.com>
2019-05-04 10:52:29 +0200

    removed unnecessary nodes and functions

--- Godot/DungeonCrawlerSP/core/gui/lobby/UnitLine.gd
+++ Godot/DungeonCrawlerSP/core/gui/lobby/UnitLine.gd
@@ -4,8 +4,6 @@ const UnitCreationDatumGd    = preload("res://core/UnitCreationDatum.gd")
 
 var _lineIdx       setget deleted, getIdx
 
-signal acquired(ownerId)
-signal released()
 signal deletePressed( lineIdx )
 
 
@@ -17,28 +15,13 @@ func initialize( idx ):
 	_lineIdx = idx
 
 
-func acquire( playerId ):
-	get_node("Owner").text = str(playerId)
-	get_node("Acquire").hide()
-	emit_signal("acquired", playerId)
-	get_node("Delete").set_disabled(false)
-
-
-func release( playerId ):
-	get_node("Owner").text = "0"
-	get_node("Acquire").show()
-	get_node("Release").hide()
-	emit_signal("released")
-	get_node("Delete").set_disabled(true)
-
-
 func setUnit( unitDatum : UnitCreationDatumGd ):
 	get_node("Name").text = unitDatum.name
 	$"TextureRect".texture = unitDatum.icon
 
 
 func onDeletePressed():
-	emit_signal("deletePressed", get_index())
+	emit_signal( "deletePressed", get_index() )
 
 
 func getIdx():
