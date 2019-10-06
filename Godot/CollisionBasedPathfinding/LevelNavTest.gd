04077aa3c25c7c9cd9d4a49f287f2778703d6551
hilfazer <az13337@gmail.com>
2019-10-06 12:32:53 +0200

    multiple AStar grids

--- Godot/CollisionBasedPathfinding/LevelNavTest.gd
+++ Godot/CollisionBasedPathfinding/LevelNavTest.gd
@@ -3,22 +3,30 @@ extends CanvasItem
 const CellSize = Vector2(32, 32)
 
 var _path : PoolVector3Array
-onready var _astar1 = $'AStar1'
-onready var _body1 = $'Body1'
+
+onready var _sectorNodes = [
+	[$'Sector1', $'Body1', $'AStar1'],
+	[$'Sector2', $'Body2', $'AStar2'],
+	]
 
 
 func _ready():
-	var tileRect = _calculateLevelRect(CellSize, [$"Sector1"])
+	for nodes in _sectorNodes:
+		var sector = nodes[0]
+		var body = nodes[1]
+		var astar = nodes[2]
 
-	var boundingRect = Rect2(
-		tileRect.position.x * CellSize.x +1,
-		tileRect.position.y * CellSize.y +1,
-		tileRect.size.x * CellSize.x -1,
-		tileRect.size.y * CellSize.y -1
-		)
+		var tileRect = _calculateLevelRect(CellSize, [sector])
 
-	_astar1.initialize(CellSize, boundingRect, _body1.get_node('CollisionShape2D'))
-	_createGraph()
+		var boundingRect = Rect2(
+			tileRect.position.x * CellSize.x +1,
+			tileRect.position.y * CellSize.y +1,
+			tileRect.size.x * CellSize.x -1,
+			tileRect.size.y * CellSize.y -1
+			)
+
+		astar.initialize(CellSize, boundingRect, body.get_node('CollisionShape2D'))
+		_createGraph(astar)
 
 
 func _input(event):
@@ -29,21 +37,24 @@ func _input(event):
 func _draw():
 #	return
 
-	draw_rect( _astar1.getBoundingRect(), Color.blue, false )
+	for nodes in _sectorNodes:
+		var astar = nodes[2]
 
-	for edge in _astar1.getAStarEdges2D():
-		draw_line(edge[0], edge[1], Color.purple, 1.0)
+		draw_rect( astar.getBoundingRect(), Color.blue, false )
 
-	for point in _astar1.getAStarPoints2D():
-		draw_circle(point, 1, Color.cyan)
+		for edge in astar.getAStarEdges2D():
+			draw_line(edge[0], edge[1], Color.purple, 1.0)
 
-	for i in range(0, _path.size() - 1):
-		draw_line(Vector2(_path[i].x, _path[i].y), Vector2(_path[i+1].x, _path[i+1].y) \
-			, Color.yellow, 1.5)
+		for point in astar.getAStarPoints2D():
+			draw_circle(point, 1, Color.cyan)
+
+		for i in range(0, _path.size() - 1):
+			draw_line(Vector2(_path[i].x, _path[i].y), Vector2(_path[i+1].x, _path[i+1].y) \
+				, Color.yellow, 1.5)
 
 
 func _calculateLevelRect( targetSize : Vector2, tilemapList : Array ) -> Rect2:
-	var levelRect := Rect2()
+	var levelRect : Rect2
 
 	for tilemap in tilemapList:
 		assert(tilemap is TileMap)
@@ -51,13 +62,17 @@ func _calculateLevelRect( targetSize : Vector2, tilemapList : Array ) -> Rect2:
 		var tilemapTargetRatio = tilemap.cell_size / targetSize * tilemap.scale
 		usedRect.position *= tilemapTargetRatio
 		usedRect.size *= tilemapTargetRatio
-		levelRect = levelRect.merge(usedRect)
+
+		if not levelRect:
+			levelRect = usedRect
+		else:
+			levelRect = levelRect.merge(usedRect)
 
 	return levelRect
 
 
-func _createGraph():
-	_astar1.createGraph()
+func _createGraph(astar):
+	astar.createGraph()
 
 #
 #func _findPath():
