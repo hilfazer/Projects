ff428d56f943a4e4e225a34092d57600df7f54d6
hilfazer <az13337@gmail.com>
2020-05-25 19:59:18 +0200

    test_saveAndLoadChildrenOrder():

---
+++ Godot/Serialization/tests/Test_ChildrenOrder.gd
@@ -0,0 +1,46 @@
+extends "res://addons/gut/test.gd"
+
+const SerializerGd           = preload("res://HierarchicalSerializer.gd")
+const Scene1Scn              = preload("res://tests/Scene1.tscn")
+
+
+func test_saveAndLoadChildrenOrder():
+	randomize()
+	var saveFile = "user://saveAndLoadChildrenOrder.tres"
+	var namesArray := []
+	var intsArray := []
+
+	for i in range(30):
+		var randomNumber = randi() % 10000
+		namesArray.append( str(randomNumber) )
+		intsArray.append( i )
+
+	yield( get_tree(), "idle_frame" )
+
+	var topChild = Scene1Scn.instance()
+	topChild.name = "topChild"
+	add_child( topChild )
+
+	for i in range( namesArray.size() ):
+		var node = Scene1Scn.instance()
+		node.name = namesArray[i]
+		node.ii = intsArray[i]
+		topChild.add_child( node )
+
+	var serializer = SerializerGd.new()
+	serializer.addSerialized( "topKey", SerializerGd.serialize( topChild ) )
+	serializer.saveToFile( saveFile )
+
+	topChild.name = "oldChild"
+	serializer.loadFromFile( saveFile )
+	serializer.deserialize( serializer.getSerialized("topKey"), self )
+
+	var loadedNamesArray := []
+	var loadedIntsArray := []
+	for child in $"topChild".get_children():
+		loadedNamesArray.append( child.name )
+		loadedIntsArray.append( child.ii )
+
+	assert_eq( namesArray, loadedNamesArray )
+	assert_eq( intsArray, loadedIntsArray )
+
