44934054743fb79c361c059bee5f37d8820aa60a
hilfazer <az13337@gmail.com>
2020-06-09 21:38:22 +0200

    added passing parameters through metadata

--- Godot/SceneSwitcher/Scene.gd
+++ Godot/SceneSwitcher/Scene.gd
@@ -2,16 +2,16 @@ extends Control
 
 const SwitchText = "switchScene( %s : filepath )"
 const SwitchToText = "switchScene( %s : PackedScene )"
+const META_PARAM = "META"
 
 export(String) var nextScene = ""
 export(String) var defaultParamText = ""
 
-var m_param
-
+var paramFromSwitcher
 
 func _init():
-	m_param = SceneSwitcher.getParams()
-	print( "Scene.gd _init(). params: ", m_param )
+	paramFromSwitcher = SceneSwitcher.getParams()
+	print( "Scene.gd _init(). params: ", paramFromSwitcher )
 
 
 func _enter_tree():
@@ -25,31 +25,41 @@ func _ready():
 	$"VBoxButtons/SwitchTo".text = SwitchToText % nextScene
 	$"VBoxParam/LineEditInput".text = defaultParamText
 
-	if m_param != null:
-		$"VBoxParam/LineEditReceived".text = m_param
+	if paramFromSwitcher != null:
+		$"VBoxParam/LineEditReceived".text = paramFromSwitcher
 	else:
 		$"VBoxParam/LineEditReceived".text = defaultParamText
 
+	if has_meta(META_PARAM):
+		if get_meta(META_PARAM) != null:
+			$"VBoxParam/LineEditReceivedMeta".text = get_meta(META_PARAM)
+		else:
+			$"VBoxParam/LineEditReceivedMeta".text = defaultParamText
+
 
 func switchPath():
-	SceneSwitcher.switchScene(nextScene, $"VBoxParam/LineEditInput".text)
+	SceneSwitcher.switchScene(nextScene, $"VBoxParam/LineEditInput".text, META_PARAM )
 
 
 func switchPackedScene():
 	var sceneNode = load(nextScene).instance()
-	assert( sceneNode.m_param == null )
+	assert( sceneNode.paramFromSwitcher == null )
 	var packedScene = PackedScene.new()
 	packedScene.pack( sceneNode )
-	SceneSwitcher.switchSceneTo( packedScene, $"VBoxParam/LineEditInput".text)
+	SceneSwitcher.switchSceneTo( packedScene, $"VBoxParam/LineEditInput".text, META_PARAM )
 
 
 func switchInstancedScene():
+	var metaName = "switchInstancedScene"
 	var sceneNode = load(nextScene).instance()
-	assert( sceneNode.m_param == null )
+	assert( sceneNode.paramFromSwitcher == null )
 # warning-ignore:return_value_discarded
 	SceneSwitcher.connect( "sceneSetAsCurrent", sceneNode, "_grabSceneParams" \
 		, [], CONNECT_ONESHOT )
-	SceneSwitcher.switchSceneToInstance( sceneNode, $"VBoxParam/LineEditInput".text )
+# warning-ignore:return_value_discarded
+	SceneSwitcher.connect("sceneSetAsCurrent", sceneNode, "_retrieveMeta" \
+		, [metaName], CONNECT_ONESHOT )
+	SceneSwitcher.switchSceneToInstance( sceneNode, $"VBoxParam/LineEditInput".text, metaName )
 
 
 func reloadScene():
@@ -63,5 +73,20 @@ func switchNull():
 
 func _grabSceneParams():
 	assert( self == get_tree().current_scene )
-	m_param = SceneSwitcher.getParams()
+	paramFromSwitcher = SceneSwitcher.getParams()
 
+
+func _retrieveMeta( meta : String ):
+	if not has_meta( meta ):
+		return
+
+	var param = get_meta( meta )
+
+	if param != null:
+		$"VBoxParam/LineEditReceivedMeta".text = param
+	else:
+		$"VBoxParam/LineEditReceivedMeta".text = defaultParamText
+
+
+func _retrieveMetaWithScene( _scene, meta : String ):
+	_retrieveMeta( meta )
