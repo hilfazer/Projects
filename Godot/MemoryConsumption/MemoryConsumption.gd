50ecb5f676941f77792e42b2246b53fe1b67fbfa
hilfazer <az13337@gmail.com>
2020-06-11 13:30:50 +0200

    ints and pool ints work

--- Godot/MemoryConsumption/MemoryConsumption.gd
+++ Godot/MemoryConsumption/MemoryConsumption.gd
@@ -10,7 +10,31 @@ var ress := []
 var pints := PoolIntArray()
 var nods := []
 
-onready var amount = $"ObjectAmount" as SpinBox
+onready var amount                     = $"ObjectAmount" as SpinBox
+onready var intsTimeLabel              = $"Lines/integer/TimeTaken"
+onready var poolIntsTimeLabel          = $"Lines/pool integer/TimeTaken"
+onready var objectsTimeLabel           = $"Lines/object/TimeTaken"
+onready var referencesTimeLabel        = $"Lines/reference/TimeTaken"
+onready var resourcesTimeLabel         = $"Lines/resource/TimeTaken"
+onready var nodesTimeLabel             = $"Lines/node/TimeTaken"
+
+
+onready var type2timeLabel = {
+	Type.Int : intsTimeLabel,
+	Type.PoolInt : poolIntsTimeLabel,
+	Type.Ref : referencesTimeLabel,
+	Type.Res : resourcesTimeLabel,
+	Type.Obj : objectsTimeLabel,
+	Type.Nod : nodesTimeLabel,
+}
+onready var type2name = {
+	Type.Int : "ints",
+	Type.PoolInt : "pool ints",
+	Type.Obj : "objects",
+	Type.Ref : "references",
+	Type.Res : "resources",
+	Type.Nod : "nodes",
+}
 
 
 signal intsCountChanged( count )
@@ -20,12 +44,20 @@ signal referencesCountChanged( count )
 signal resourcesCountChanged( count )
 signal nodesCountChanged( count )
 
+signal creationTime( type, timeMs, size )
+signal computationTime( type, timeMs, size )
+
 
 func _init():
 	OS.set_window_always_on_top(true)
 
 
 func _ready():
+# warning-ignore:return_value_discarded
+	connect("computationTime", self, "_updateComputationTime")
+# warning-ignore:return_value_discarded
+	connect("creationTime", self, "_updateCreationTime")
+
 # warning-ignore:return_value_discarded
 	$"Lines/integer/ButtonType".connect("toggled", self, "_on_ButtonInts_toggled")
 # warning-ignore:return_value_discarded
@@ -52,19 +84,10 @@ func _ready():
 # warning-ignore:return_value_discarded
 	connect("nodesCountChanged", $"Lines/node/Amount", "set_text" )
 
-
-func create( type : int ):
-	match type:
-		Type.Ref:
-			return Reference.new()
-		Type.Res:
-			return Resource.new()
-		Type.Obj:
-			return MyObj.new()
-		Type.Int:
-			return 3
-		Type.Nod:
-			return Node.new()
+# warning-ignore:return_value_discarded
+	$"Lines/integer/ButtonCompute".connect("pressed", self, "_on_ButtonAddInts_pressed")
+# warning-ignore:return_value_discarded
+	$"Lines/pool integer/ButtonCompute".connect("pressed", self, "_on_ButtonAddPoolInts_pressed")
 
 
 class MyObj extends Object:
@@ -78,28 +101,36 @@ class MyRef extends Reference:
 
 
 func _on_ButtonInts_toggled(button_pressed):
+	ints = []
+
+	var msecStart = OS.get_ticks_msec()
 	if button_pressed:
 		ints.resize( int(amount.value) )
 		for i in int(amount.value):
 			ints[i] = 3
-	else:
-		ints = []
+
+	var msecEnd = OS.get_ticks_msec() - msecStart
 	emit_signal("intsCountChanged", str( ints.size() ) )
+	emit_signal("creationTime", Type.Int, msecEnd, ints.size() )
 
 
 func _on_ButtonPoolInts_toggled(button_pressed):
+	pints.resize( 0 )
+
+	var msecStart = OS.get_ticks_msec()
 	if button_pressed:
 		pints.resize( int(amount.value) )
 		for i in int(amount.value):
 			pints[i] = 3
-	else:
-		pints.resize( 0 )
+
+	var msecEnd = OS.get_ticks_msec() - msecStart
 	emit_signal("poolIntsCountChanged", str( pints.size() ) )
+	emit_signal("creationTime", Type.PoolInt, msecEnd, pints.size() )
 
 
 func _on_ButtonObjects_toggled(button_pressed):
 	if button_pressed:
-		objs.resize( int(amount.value) )  # amount.value was 100000
+		objs.resize( int(amount.value) )
 		for i in int(amount.value):
 			objs[i] = MyObj.new()
 	else:
@@ -148,9 +179,8 @@ func _on_ButtonAddInts_pressed():
 	for i in range(ints.size()):
 		_sum += ints[i]
 
-
-	print( "adding %s ints took %s msec" % [ints.size(), OS.get_ticks_msec() - msecStart] )
-
+	var msecEnd = OS.get_ticks_msec() - msecStart
+	emit_signal("computationTime", Type.Int, msecEnd, ints.size() )
 
 func _on_ButtonAddPoolInts_pressed():
 	var msecStart = OS.get_ticks_msec()
@@ -159,4 +189,19 @@ func _on_ButtonAddPoolInts_pressed():
 	for i in range(pints.size()):
 		_sum += pints[i]
 
-	print( "adding %s pool ints took %s msec" % [pints.size(), OS.get_ticks_msec() - msecStart] )
+	var msecEnd = OS.get_ticks_msec() - msecStart
+	emit_signal("computationTime", Type.PoolInt, msecEnd, pints.size() )
+
+
+func _updateComputationTime( type : int, timeMs : int, size : int ):
+	var message = "computing %s: %s msec"
+	type2timeLabel[type].text = message % [size, timeMs]
+
+
+func _updateCreationTime( type : int, timeMs : int, size : int ):
+	var message = "creating %s: %s msec"
+	type2timeLabel[type].text = message % [size, timeMs]
+
+
+
+
