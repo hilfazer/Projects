5a602bbfc17e5f36dd514f6e5553a77d88ef0a7d
hilfazer <az13337@gmail.com>
2021-03-13 20:31:35 +0100

    PoolIntLine

---
+++ Godot/MemoryConsumption/PoolIntLine.gd
@@ -0,0 +1,22 @@
+extends "res://AbstractTypeLine.gd"
+
+
+var poolInts := PoolIntArray()
+
+
+func _create( count : int ) -> int:
+	poolInts.resize(count)
+	for i in poolInts.size():
+		poolInts[i] = 3
+	return OK
+
+
+func _destroy():
+	poolInts.resize(0)
+
+
+func _compute():
+	var _sum := 0
+	for i in range(poolInts.size()):
+		_sum += poolInts[i]
+
