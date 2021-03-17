419ed4cf0b54506137700bcbe88e4fc26a8e2aa7
hilfazer <az13337@gmail.com>
2021-03-17 17:45:25 +0100

    typed arrays update

--- Godot/EngineIssues/TypedArrays_40/Node.gd
+++ Godot/EngineIssues/TypedArrays_40/Node.gd
@@ -2,25 +2,44 @@ extends Node
 
 
 func _ready():
-	#var incorrectDeclaration: Array[int] = [1, "6", 3.6, null, "s2", AABB()]
+	var incorrectDeclaration: Array[int] = [1, "6", 3.6, null, "s2", AABB()] # not allowed, good
 
-	var objects: Array[Object] = [null, Node.new()]
+# this produces "_ready: Class names can only be set for type OBJECT" errors
+# i get such typed arrays are not supported
+	var objects: Array[Object]
+	var nodes2d: Array[Node2D]
 
-	var nodes2d: Array[Node2D]= [Node2D.new(), Sprite2D.new(), Node.new()]
-	nodes2d.append( Node.new() )
-
-	var regularArray :Array[float]= [1.6,2.6]
+	var regularArray :Array= [1.6,2.6] # explicit typing is honoured
 	regularArray.append(Plane())
-
+	assert( typeof(regularArray.back()) == TYPE_PLANE )
 
 	var arrint: Array[int] = [1,2]
 	arrint.append(['f'])
 	arrint.append(null)
 	arrint.append([6])
+	arrint.append(9)
+	assert(arrint.size() == 3)	# only int got appended, good
 
-	var withString = appendString( [4,5] )
+	var withString = appendString( [4,5] ) # it's treated like a regular array, good
 	assert(withString.back() == "d")
+
+	var floatArray :Array = makeFloats()
+	print(floatArray)
+
+	var array = makeArray()
+	array.append([])
+	print(array)
+	assert(array.size() == 3)
 
 
 func appendString( ar: Array ):
 	ar.append("d")
+	return ar
+
+
+func makeFloats() -> Array[float]:
+	return [.4, .5, 's']	# typed array is not enforced for the return type
+
+
+func makeArray():
+	return [.1,.2] # returns a regular array, good
