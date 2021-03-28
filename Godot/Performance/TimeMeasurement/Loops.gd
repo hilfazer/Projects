efe94861db61cb8d32038ae9bf43041912bd6322
hilfazer <az13337@gmail.com>
2021-03-28 16:10:56 +0200

    rolled vs unrolled for

--- Godot/Performance/TimeMeasurement/Loops.gd
+++ Godot/Performance/TimeMeasurement/Loops.gd
@@ -4,9 +4,13 @@ extends "./TimeMeasureScene.gd"
 
 func _ready():
 # warning-ignore:return_value_discarded
-	addMeasure(ForLoop.new(), "ForLoop")
+	addMeasure(ForLoop.new(),     "ForLoop")
 # warning-ignore:return_value_discarded
 	addMeasure(WhileLoop.new(),   "WhileLoop")
+# warning-ignore:return_value_discarded
+	addMeasure(RolledFor.new(),   "RolledFor")
+# warning-ignore:return_value_discarded
+	addMeasure(UnrolledFor.new(), "UnrolledFor")
 
 
 class ForLoop extends MeasureBase:
@@ -27,3 +31,26 @@ class WhileLoop extends MeasureBase:
 				pass
 				y += 1
 			x += 1
+
+
+class RolledFor extends MeasureBase:
+	func _execute():
+		var array := [0, 0, 0, 0]
+
+		for i in loopCount / 2:
+			for j in loopCount / 2:
+				for k in [0, 1]:
+					array[2*k] += 1
+					array[2*k + 1] += 1
+
+
+class UnrolledFor extends MeasureBase:
+	func _execute():
+		var array := [0, 0, 0, 0]
+
+		for i in loopCount / 2:
+			for j in loopCount / 2:
+				array[0] += 1
+				array[1] += 1
+				array[2] += 1
+				array[3] += 1
