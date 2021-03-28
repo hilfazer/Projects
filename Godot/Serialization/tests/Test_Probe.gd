587fa15af027b646b6b138256f759d1ab779398d
hilfazer <az13337@gmail.com>
2021-03-28 19:49:06 +0200

    autofree()

--- Godot/Serialization/tests/Test_Probe.gd
+++ Godot/Serialization/tests/Test_Probe.gd
@@ -6,15 +6,14 @@ const ProbeGd                = preload("res://Probe.gd")
 
 
 func test_probeValidSubtree():
-	var scene1 = Scene1Scn.instance()
+	var scene1 = autofree( Scene1Scn.instance() )
 	var probe = ProbeGd.scan( scene1 )
 	assert_eq( probe.nodesNotInstantiable.size(), 0 )
 	assert_eq( probe.nodesNoMatchingDeserialize.size(), 0 )
-	scene1.free()
 
 
 func test_noninstantiableSubtree():
-	var scene1 = Scene1Scn.instance()
+	var scene1 = autofree( Scene1Scn.instance() )
 	var node = Node.new()
 	node.name = "CantInstance"
 	scene1.add_child( node )
@@ -24,18 +23,16 @@ func test_noninstantiableSubtree():
 
 	if probe.nodesNotInstantiable.size() == 1:
 		assert_eq( probe.nodesNotInstantiable[0].name, "CantInstance" )
-	scene1.free()
 
 
 func test_nonserializableSubtree():
-	var scene1 = SceneNoDeserializeGd.instance()
+	var scene1 = autofree( SceneNoDeserializeGd.instance() )
 	var probe = ProbeGd.scan( scene1 )
 	assert_eq( probe.nodesNotInstantiable.size(), 0 )
 	assert_eq( probe.nodesNoMatchingDeserialize.size(), 1 )
 
 	if probe.nodesNoMatchingDeserialize.size() == 1:
 		assert_eq( probe.nodesNoMatchingDeserialize[0].name, "NoDeserialize" )
-	scene1.free()
 
 
 func test_NonInstantiableOutsideTree():
