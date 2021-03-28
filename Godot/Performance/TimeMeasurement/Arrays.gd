abefda9a5dab0b6077fd6d6dfbc66f89d00b3df4
hilfazer <az13337@gmail.com>
2021-03-28 19:34:50 +0200

    array and dictionary indexing

--- Godot/Performance/TimeMeasurement/Arrays.gd
+++ Godot/Performance/TimeMeasurement/Arrays.gd
@@ -5,9 +5,11 @@ extends "./TimeMeasureScene.gd"
 func _ready():
 	addMeasure(ArrayDoubleIndexing.new(), "ArrayDoubleIndexing")
 	addMeasure(ArraySingleIndexing.new(), "ArraySingleIndexing")
+	addMeasure(ArrayWithEnumKeys.new(), "ArrayWithEnumKeys")
+	addMeasure(DictionaryWithStringKeys.new(), "DictionaryWithStringKeys")
 
 
-class BaseWithArray extends MeasureBase:
+class BaseWith2DArray extends MeasureBase:
 	var array = []
 
 	func setLoopCount(count : int):
@@ -21,16 +23,66 @@ class BaseWithArray extends MeasureBase:
 			array[i] = inner.duplicate()
 
 
-class ArrayDoubleIndexing extends BaseWithArray:
+class ArrayDoubleIndexing extends BaseWith2DArray:
 	func _execute():
 		for i in array.size():
 			for j in array[i].size():
 				var _bag = array[i][j]
 
 
-class ArraySingleIndexing extends BaseWithArray:
+class ArraySingleIndexing extends BaseWith2DArray:
 	func _execute():
 		for i in array.size():
 			var inner : Array = array[i]
 			for j in inner.size():
 				var _bag = inner[j]
+
+
+enum Keys { One, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten }
+const StringKeys = ['One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten']
+
+
+class ArrayWithEnumKeys extends MeasureBase:
+	var array := []
+	var keysToCheck := []
+
+
+	func _init():
+		for i in Keys.size():
+			array.append(StringKeys[i])
+
+
+	func setLoopCount(count : int):
+		.setLoopCount(count)
+
+		keysToCheck.resize(count)
+		for i in count:
+			keysToCheck[i] = Keys.values()[ randi() % Keys.size() ]
+
+
+	func _execute():
+		for key in keysToCheck:
+			array[key]
+
+
+class DictionaryWithStringKeys extends MeasureBase:
+	var dict := {}
+	var keysToCheck := []
+
+
+	func _init():
+		for i in Keys.size():
+			dict[StringKeys[i]] = StringKeys[i]
+
+
+	func setLoopCount(count : int):
+		.setLoopCount(count)
+
+		keysToCheck.resize(count)
+		for i in count:
+			keysToCheck[i] = StringKeys[ randi() % StringKeys.size() ]
+
+
+	func _execute():
+		for key in keysToCheck:
+			dict[key]
