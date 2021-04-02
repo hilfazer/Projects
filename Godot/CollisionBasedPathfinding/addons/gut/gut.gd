5dec50d4983d9476e570867662a980c86e5ebbcb
hilfazer <az13337@gmail.com>
2021-04-02 20:44:59 +0200

    multicolour GUT summary

--- Godot/CollisionBasedPathfinding/addons/gut/gut.gd
+++ Godot/CollisionBasedPathfinding/addons/gut/gut.gd
@@ -374,15 +374,17 @@ func _print_summary():
 	_lgr.log(logger_text)
 
 	if(_new_summary.get_totals().tests > 0):
-		var fmt = _lgr.fmts.green
-		var msg = str(_new_summary.get_totals().passing) + ' passed ' + str(_new_summary.get_totals().failing) + ' failed.  ' + \
-			str("Tests finished in ", _gui.elapsed_time_as_str())
-		if(_new_summary.get_totals().failing > 0):
-			fmt = _lgr.fmts.red
-		elif(_new_summary.get_totals().pending > 0):
+		var fmt = _lgr.fmts.none
+		var msgPassing = str(_new_summary.get_totals().passing) + ' passed '
+		var msgFailing = str(_new_summary.get_totals().failing) + ' failed.  '
+		var msgFinished = str("Tests finished in ", _gui.elapsed_time_as_str()) + "\n"
+
+		if(_new_summary.get_totals().pending > 0):
 			fmt = _lgr.fmts.yellow
 
-		_lgr.log(msg, fmt)
+		_lgr.lograw(msgPassing, _lgr.fmts.green if _new_summary.get_totals().passing else _lgr.fmts.red)
+		_lgr.lograw(msgFailing, _lgr.fmts.red if _new_summary.get_totals().failing else _lgr.fmts.green)
+		_lgr.lograw(msgFinished, _lgr.fmts.yellow if _new_summary.get_totals().pending > 0 else fmt)
 	else:
 		_lgr.log('No tests ran', _lgr.fmts.red)
 
