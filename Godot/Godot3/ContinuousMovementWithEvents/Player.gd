0744ece06c8f9eb72c60058c9a07e8bb843580c1
hilfazer <az13337@gmail.com>
2022-03-28 19:03:13 +0200

    movement

---
+++ Godot/Godot3/ContinuousMovementWithEvents/Player.gd
@@ -0,0 +1,26 @@
+extends KinematicBody2D
+
+
+export var speed : int = 300
+
+var _movementVector := Vector2.ZERO
+
+
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_up") or event.is_action_released("ui_down"):
+		_updateMovementVector(Vector2.UP)
+	elif event.is_action_released("ui_up") or event.is_action_pressed("ui_down"):
+		_updateMovementVector(Vector2.DOWN)
+	elif event.is_action_pressed("ui_left") or event.is_action_released("ui_right"):
+		_updateMovementVector(Vector2.LEFT)
+	elif event.is_action_released("ui_left") or event.is_action_pressed("ui_right"):
+		_updateMovementVector(Vector2.RIGHT)
+
+
+
+func _physics_process(delta):
+	move_and_collide(_movementVector.normalized() * delta * speed)
+
+
+func _updateMovementVector(direction : Vector2) -> void:
+	_movementVector += direction
