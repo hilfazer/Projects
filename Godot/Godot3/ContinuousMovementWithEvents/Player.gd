d2f1ba1666fa16080528a490b4c9d434cb84917c
hilfazer <az13337@gmail.com>
2022-03-29 19:51:47 +0200

    a variable for each direction

--- Godot/Godot3/ContinuousMovementWithEvents/Player.gd
+++ Godot/Godot3/ContinuousMovementWithEvents/Player.gd
@@ -4,23 +4,37 @@ extends KinematicBody2D
 export var speed : int = 300
 
 var _movementVector := Vector2.ZERO
+var _up := 0
+var _down := 0
+var _left := 0
+var _right := 0
 
 
 func _unhandled_input(event):
 	if event.is_action_type():
 		print_debug(event.as_text())
 
-	if event.is_action_pressed("ui_up") or event.is_action_released("ui_down"):
-		_updateMovementVector(Vector2.UP)
-	elif event.is_action_released("ui_up") or event.is_action_pressed("ui_down"):
-		_updateMovementVector(Vector2.DOWN)
-	elif event.is_action_pressed("ui_left") or event.is_action_released("ui_right"):
-		_updateMovementVector(Vector2.LEFT)
-	elif event.is_action_released("ui_left") or event.is_action_pressed("ui_right"):
-		_updateMovementVector(Vector2.RIGHT)
+	if event.is_action_pressed("ui_up"):
+		_up = 1
+	elif event.is_action_released("ui_up"):
+		_up = 0
+	elif event.is_action_pressed("ui_down"):
+		_down = 1
+	elif event.is_action_released("ui_down"):
+		_down = 0
+	elif event.is_action_pressed("ui_left"):
+		_left = 1
+	elif event.is_action_released("ui_left"):
+		_left = 0
+	elif event.is_action_pressed("ui_right"):
+		_right = 1
+	elif event.is_action_released("ui_right"):
+		_right = 0
+
 	else:
 		return
 
+	_updateMovementVector()
 	get_tree().set_input_as_handled()
 
 
@@ -29,5 +43,6 @@ func _physics_process(delta):
 	move_and_collide(_movementVector.normalized() * delta * speed)
 
 
-func _updateMovementVector(direction : Vector2) -> void:
-	_movementVector += direction
+func _updateMovementVector() -> void:
+	_movementVector.x = _right - _left
+	_movementVector.y = _down - _up
