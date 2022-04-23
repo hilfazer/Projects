5d821feacf813e790bb6b7e20be92cb63819d03d
hilfazer <az13337@gmail.com>
2022-04-23 11:47:30 +0200

    writing and reading after load()

--- Godot/Godot3/SavingScenesAndResources/resource_sharing/AreResourcesShared.gd
+++ Godot/Godot3/SavingScenesAndResources/resource_sharing/AreResourcesShared.gd
@@ -5,11 +5,19 @@ const res_reader = preload("res://resource_sharing/resource_reader.gd")
 
 func _ready():
 	var writer = res_writer.new()
-	writer.preload_and_write()
 
+	writer.preload_and_write()
 	call_deferred("read_with_preload")
+
+	writer.load_and_write()
+	call_deferred("read_with_load")
 
 
 func read_with_preload():
 	var reader = res_reader.new()
 	reader.preload_and_read()
+
+
+func read_with_load():
+	var reader = res_reader.new()
+	reader.load_and_read()
