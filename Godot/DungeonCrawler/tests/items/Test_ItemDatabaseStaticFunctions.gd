552dbab86b784a6b628c170385a576d2012c35c3
hilfazer <az13337@gmail.com>
2022-05-03 10:08:04 +0200

    Test_ItemDatabaseStaticFunctions.gd

---
+++ Godot/DungeonCrawler/tests/items/Test_ItemDatabaseStaticFunctions.gd
@@ -0,0 +1,9 @@
+extends "res://tests/GutTestBase.gd"
+
+const ItemDbFactoryGd        = preload("res://engine/items/ItemDbFactory.gd")
+const EmptyItemDatabasePath  = "res://tests/items/DB0/EmptyItemDB.gd"
+const ItemDatabase1Path      = "res://tests/items/DB1/ItemDB1.gd"
+
+
+func test_itemDatabaseCheckForDuplicates():
+	pending()
