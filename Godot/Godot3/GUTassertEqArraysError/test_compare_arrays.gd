23646618b8c41b8ee326887badeb6b146382064b
hilfazer <az13337@gmail.com>
2022-05-03 13:18:35 +0200

    test_assert_eq_deep_array_to_pool_array()

--- Godot/Godot3/GUTassertEqArraysError/test_compare_arrays.gd
+++ Godot/Godot3/GUTassertEqArraysError/test_compare_arrays.gd
@@ -1,25 +1,27 @@
 extends GutTest
 
-
-var params = ParameterFactory.named_parameters( ['array'],
-	[
-		[ ['p'] ]
-	]
-)
-
-
-func test_assert_eq_shallow_array_from_param_to_pool_array(p = use_parameters(params)):
+func test_assert_eq_shallow_array_to_pool_array():
 	var pool_array = PoolByteArray()
-	assert_eq_shallow(p.array, pool_array)
+	assert_eq_shallow([], pool_array)
 
-
-func test_assert_eq_shallow_pool_array_to_array_from_param(p = use_parameters(params)):
+func test_assert_eq_shallow_pool_array_to_array():
 	var pool_array = PoolByteArray()
-	assert_eq_shallow(pool_array, p.array)
+	assert_eq_shallow(pool_array, [8,0])
 
 
-func test_compare(p = use_parameters(params)):
+func test_assert_eq_deep_array_to_pool_array():
+	var pool_array = PoolByteArray()
+	assert_eq_deep([], pool_array)
+
+
+func test_compare_array_to_pool_array():
 	var pool_array = PoolByteArray()
 	var cmp = compare_shallow(['cxz'], pool_array)
-	print( cmp.summary != null )
 	assert_ne( cmp.summary, null )
+
+
+func test_compare_pool_array_to_array():
+	var pool_array = PoolByteArray()
+	var cmp = compare_shallow(pool_array, ['cxz'])
+	assert_ne( cmp.summary, null )
+
